de8d31d58c7b8d644cd94b18d628cf02b67a5e51
Zongmin Lei <leizongmin@gmail.com>
2016-06-22 14:53:01 +0800

    简化项目结构

--- app.js
+++ app.js
@@ -1,60 +1,55 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
-var app = express();
+// 载入路由处理程序
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
-// view engine setup
+// 创建express实例
+const app = express();
+
+// 设置模板引擎
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// 载入和初始化中间件
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 注册路由
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// 404页面
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// 出错页面
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: app.get('env') === 'development' ? err : {},
   });
 });
 
-
-module.exports = app;
+// 监听端口
+const port = process.env.PORT || 3000;
+app.listen(port, err => {
+  if (err) throw err;
+  console.log('服务器已启动。  http://127.0.0.1:%d', port);
+});
